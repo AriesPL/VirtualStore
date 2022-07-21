1302c40b50274d5cb2dad0b4843c90613184483d
AriesPL <blackhorns1989@gmail.com>
2022-07-21 12:51:55 +0300

    feat: Класс работника и его представление

--- VirtualStore/Controllers/HomeController.cs
+++ VirtualStore/Controllers/HomeController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VirtualStore.Models;
 
 namespace VirtualStore.Controllers
 {
 	public class HomeController : Controller
 	{
+		private static readonly List<Employee> employees = new()
+		{
+			new Employee{Id = 1,Name = "Alex",SecondName = "Sergeev", Age = 33},
+			new Employee{Id = 2,Name = "Natalya",SecondName = "Sergeeva", Age = 62},
+			new Employee{Id = 3,Name = "Andrey",SecondName = "Sergeev", Age = 39},
+		};
+
 		public IActionResult Index()
 		{
 			return View();
+		}
+
+		public IActionResult Result()
+		{
+			return Content("Hi");
+		}
+
+		public IActionResult Employees()
+		{
+			return View(employees);
 		}
 	}
 }
