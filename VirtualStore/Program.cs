3659813929f5603e8549e280468542bab7c2e58e
AriesPL <blackhorns1989@gmail.com>
2022-07-21 11:51:10 +0300

    feat: Добавлено представление страницы Index

--- VirtualStore/Program.cs
+++ VirtualStore/Program.cs
@@ -1,6 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var service = builder.Services;
+service.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => app.Configuration["CustomerGreeting"]);
+if (app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+
+//app.MapGet("/", () => app.Configuration["CustomerGreeting"]);
+
+app.MapGet("/throw", () =>
+{
+	throw new ApplicationException("Error");
+});
+
+
+
+//app.MapDefaultControllerRoute();
+
+app.MapControllerRoute(name:"default", pattern:"{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
