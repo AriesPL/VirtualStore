4de133fedfc1c4e20596fad97bec73bda7e1ed1a
AriesPL <blackhorns1989@gmail.com>
2022-08-13 10:37:55 +0300

    Создан отдельный контролер и представление для Сотрудников

---
+++ VirtualStore/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VirtualStore.Models;
+
+namespace VirtualStore.Controllers
+{
+	public class EmployeesController : Controller
+	{
+		private static readonly List<Employee> employees = new()
+		{
+			new Employee{Id = 1,Name = "Alex",SecondName = "Sergeev", Age = 33},
+			new Employee{Id = 2,Name = "Natalya",SecondName = "Sergeeva", Age = 62},
+			new Employee{Id = 3,Name = "Andrey",SecondName = "Sergeev", Age = 39},
+		};
+
+		public IActionResult Index()
+		{
+			var result = employees;
+			return View(result);
+		}
+	}
+}
--- VirtualStore/Controllers/HomeController.cs
+++ VirtualStore/Controllers/HomeController.cs
@@ -5,13 +5,6 @@ namespace VirtualStore.Controllers
 {
 	public class HomeController : Controller
 	{
-		private static readonly List<Employee> employees = new()
-		{
-			new Employee{Id = 1,Name = "Alex",SecondName = "Sergeev", Age = 33},
-			new Employee{Id = 2,Name = "Natalya",SecondName = "Sergeeva", Age = 62},
-			new Employee{Id = 3,Name = "Andrey",SecondName = "Sergeev", Age = 39},
-		};
-
 		public IActionResult Index()
 		{
 			return View();
@@ -20,11 +13,6 @@ namespace VirtualStore.Controllers
 		public IActionResult Result()
 		{
 			return Content("Hi");
-		}
-
-		public IActionResult Employees()
-		{
-			return View(employees);
 		}
 	}
 }
