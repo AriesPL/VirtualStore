b423a1d8e5767759d14f64cf35da21092ab8dc70
AriesPL <blackhorns1989@gmail.com>
2022-08-14 10:00:02 +0300

    Работа с libman

--- VirtualStore/Models/Employee.cs
+++ VirtualStore/Models/Employee.cs
@@ -3,8 +3,9 @@
 	public class Employee
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
-		public string SecondName { get; set; }
+		public string? Name { get; set; }
+		public string? SecondName { get; set; }
 		public int Age { get; set; }
-	}
+
+    }
 }
